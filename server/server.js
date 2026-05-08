980a753f035913458e0e5348ca7041ea3fedad3e
Yadeni L. <yadeni4@gmail.com>
2026-05-08 12:08:26 -0700

    Deployment setup

--- server/server.js
+++ server/server.js
@@ -28,8 +28,8 @@ app.get('/', (req, res) => {
 });
 
 
-// const PORT = process.env.PORT || 5000;
-const PORT = 5050;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
